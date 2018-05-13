2d8c5564d18319fd120e2ee10f5e896882638698
Andrés Castellanos <andres.cv@galileo.edu>
2018-05-12 18:45:08 -0600

    remove instructions module

--- vsim/riscv/instructions/instruction.py
+++
@@ -1,8 +0,0 @@
-class Instruction:
-
-    def __init__(self, description, simcode):
-        self._description = description
-        self._simcode = simcode
-
-    def __str__(self):
-        return str(self._description)
