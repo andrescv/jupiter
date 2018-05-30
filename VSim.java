cd09be2c2ae216a0e7c605ee87f28b4c4dd201ab
Andrés Castellanos <andres.cv@galileo.edu>
2018-05-30 17:30:52 -0600

    add VSim Globals

--- VSim.java
+++ VSim.java
@@ -1,22 +1,13 @@
-import vsim.utils.*;
-import vsim.riscv.hardware.MemoryCell;
+import static vsim.Globals.*;
 
 
 public class VSim {
 
     public static void main(String[] args) {
-        MemoryCell cell = new MemoryCell(0xbaddcafe);
 
-        // signed
-        System.out.println(String.format("0x%08x", cell.loadByte(ByteOffset.FIRST)));
-        System.out.println(String.format("0x%08x", cell.loadByte(ByteOffset.SECOND)));
-        System.out.println(String.format("0x%08x", cell.loadByte(ByteOffset.THIRD)));
-        System.out.println(String.format("0x%08x", cell.loadByte(ByteOffset.FOURTH)));
-        // unsigned
-        System.out.println(String.format("0x%08x", cell.loadByteUnsigned(ByteOffset.FIRST)));
-        System.out.println(String.format("0x%08x", cell.loadByteUnsigned(ByteOffset.SECOND)));
-        System.out.println(String.format("0x%08x", cell.loadByteUnsigned(ByteOffset.THIRD)));
-        System.out.println(String.format("0x%08x", cell.loadByteUnsigned(ByteOffset.FOURTH)));
+        System.out.println(memory);
+        System.out.println();
+        System.out.println(regfile);
 
     }
 
