9651d1cb6029c2751c1f1e64a45bd1bea023a67c
Andrés Castellanos <andres.cv@galileo.edu>
2018-05-30 20:48:30 -0600

    update rtype instructions, use inheritance
    Former-commit-id: 376aa6480ca2c78ed4319f3faac51fe766376193

--- vsim/riscv/isa/instructions/rtype/Xor.java
+++ vsim/riscv/isa/instructions/rtype/Xor.java
@@ -1,19 +1,11 @@
 package vsim.riscv.isa.instructions.rtype;
 
-import vsim.Globals;
-import vsim.riscv.hardware.Register;
-import vsim.riscv.isa.instructions.Code;
-import vsim.riscv.isa.instructions.SimCode;
 
-
-public final class Xor extends SimCode {
+public final class Xor extends RType {
 
     @Override
-    public void execute(Code code) {
-        Register rs1 = Globals.regfile.getRegister(code.getRs1());
-        Register rs2 = Globals.regfile.getRegister(code.getRs2());
-        Register rd = Globals.regfile.getRegister(code.getRd());
-        rd.setValue(rs1.getValue() ^ rs2.getValue());
+    public int compute(int x, int y) {
+        return x ^ y;
     }
 
 }
