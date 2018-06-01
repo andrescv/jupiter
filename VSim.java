2b740a71b69dbe0265bb8d310832a602b1d02623
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-01 11:46:53 -0600

    update VSim.java
    Former-commit-id: 547598819b71cd23de69ecf0dd0e7de505764a4b

--- VSim.java
+++ VSim.java
@@ -1,22 +1,11 @@
 import static vsim.Globals.*;
-import vsim.riscv.isa.instructions.itype.*;
-import vsim.riscv.isa.instructions.Code;
-import vsim.riscv.isa.instructions.SimCode;
-
+import vsim.riscv.isa.InstructionSet;
 
 public class VSim {
 
     public static void main(String[] args) {
-        SimCode lwcode = new Lw();
-
-        Code c2 = new Code(0x00002303);
-
-        memory.storeWord(0xbacacafe, 0x0);
-
-        lwcode.execute(c2);
-
-        System.out.println(memory);
-        System.out.println(regfile);
+        InstructionSet isa = new InstructionSet();
+        System.out.println(isa);
     }
 
 }
