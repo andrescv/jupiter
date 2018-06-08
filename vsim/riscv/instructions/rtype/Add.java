12d544786ae4313cd492a62af279eb7366e081f2
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-08 13:25:55 -0600

    add rtype instructions
    Former-commit-id: 0a8ac96942b90ed1c3c89b7b4c50d0bbaef3fc7b

---
+++ vsim/riscv/instructions/rtype/Add.java
@@ -0,0 +1,19 @@
+package vsim.riscv.instructions.rtype;
+
+
+public final class Add extends RType {
+
+    public Add() {
+        super(
+            "add",
+            "add rd, rs1, rs2",
+            "set rd = rs1 + rs2, overflow is ignored"
+        );
+    }
+
+    @Override
+    protected int compute(int rs1, int rs2) {
+        return rs1 + rs2;
+    }
+
+}
---
+++ vsim/riscv/instructions/rtype/RType.java
@@ -0,0 +1,28 @@
+package vsim.riscv.instructions.rtype;
+
+
+import vsim.simulator.State;
+import vsim.riscv.instructions.MachineCode;
+import vsim.riscv.instructions.Instruction;
+import vsim.riscv.instructions.InstructionField;
+
+
+abstract class RType extends Instruction {
+
+  protected RType(String mnemonic, String usage, String description) {
+    super(Instruction.Format.R, mnemonic, usage, description);
+  }
+
+  protected abstract int compute(int rs1, int rs2);
+
+  @Override
+  public void execute(MachineCode code) {
+    int rs1 = State.regfile.getRegister(code.get(InstructionField.RS1));
+    int rs2 = State.regfile.getRegister(code.get(InstructionField.RS2));
+    State.regfile.setRegister(
+      code.get(InstructionField.RD),
+      this.compute(rs1, rs2)
+    );
+  }
+
+}
