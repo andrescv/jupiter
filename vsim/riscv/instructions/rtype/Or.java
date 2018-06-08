fc0a587eaa0033289fd82b5b5a6dad0735215f99
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-08 13:52:18 -0600

    fix tab length

--- vsim/riscv/instructions/rtype/Or.java
+++ vsim/riscv/instructions/rtype/Or.java
@@ -3,17 +3,17 @@ package vsim.riscv.instructions.rtype;
 
 public final class Or extends RType {
 
-    public Or() {
-        super(
-            "or",
-            "or rd, rs1, rs2",
-            "set rd = rs1 | rs2, bitwise or"
-        );
-    }
+  public Or() {
+    super(
+      "or",
+      "or rd, rs1, rs2",
+      "set rd = rs1 | rs2, bitwise or"
+    );
+  }
 
-    @Override
-    protected int compute(int rs1, int rs2) {
-        return rs1 | rs2;
-    }
+  @Override
+  protected int compute(int rs1, int rs2) {
+    return rs1 | rs2;
+  }
 
 }
--- vsim/riscv/instructions/rtype/Sub.java
+++ vsim/riscv/instructions/rtype/Sub.java
@@ -3,17 +3,17 @@ package vsim.riscv.instructions.rtype;
 
 public final class Sub extends RType {
 
-    public Sub() {
-        super(
-            "sub",
-            "sub rd, rs1, rs2",
-            "set rd = rs1 - rs2, overflow is ignored"
-        );
-    }
+  public Sub() {
+    super(
+      "sub",
+      "sub rd, rs1, rs2",
+      "set rd = rs1 - rs2, overflow is ignored"
+    );
+  }
 
-    @Override
-    protected int compute(int rs1, int rs2) {
-        return rs1 - rs2;
-    }
+  @Override
+  protected int compute(int rs1, int rs2) {
+    return rs1 - rs2;
+  }
 
 }
