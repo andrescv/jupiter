72cdab5a65af831e49a5bba324fb2f4078ccfd2e
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-08 16:21:54 -0600

    add jtype instructions
    Former-commit-id: 54c3a6505da4ae17d186f59d0464dbe5a1185545

---
+++ vsim/riscv/instructions/jtype/Jal.java
@@ -0,0 +1,23 @@
+package vsim.riscv.instructions.jtype;
+
+import vsim.simulator.State;
+
+
+public final class Jal extends JType {
+
+  public Jal() {
+    super(
+      "jal",
+      "jal rd, imm",
+      "set rd = pc + 4 and pc = pc + imm"
+    );
+  }
+
+  @Override
+  protected int compute(int imm) {
+    int pc = State.regfile.getProgramCounter();
+    State.regfile.setProgramCounter(pc + imm);
+    return pc + 4;
+  }
+
+}
