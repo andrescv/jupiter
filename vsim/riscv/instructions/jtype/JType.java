54c3a6505da4ae17d186f59d0464dbe5a1185545
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-08 16:21:54 -0600

    add jtype instructions

---
+++ vsim/riscv/instructions/jtype/JType.java
@@ -0,0 +1,31 @@
+package vsim.riscv.instructions.jtype;
+
+import vsim.utils.Data;
+import vsim.simulator.State;
+import vsim.riscv.instructions.MachineCode;
+import vsim.riscv.instructions.Instruction;
+import vsim.riscv.instructions.InstructionField;
+
+
+abstract class JType extends Instruction {
+
+  protected JType(String mnemonic, String usage, String description) {
+    super(Instruction.Format.J, mnemonic, usage, description);
+  }
+
+  protected abstract int compute(int imm);
+
+  @Override
+  public void execute(MachineCode code) {
+    int imm_10_1 = code.get(InstructionField.IMM_10_1);
+    int imm_11 = code.get(InstructionField.IMM_11J);
+    int imm_19_12 = code.get(InstructionField.IMM_19_12);
+    int imm_20 = code.get(InstructionField.IMM_20);
+    int imm = (imm_20 << 19 | imm_19_12 << 11 | imm_11 << 10 | imm_10_1) << 1;
+    State.regfile.setRegister(
+        code.get(InstructionField.RD),
+        this.compute(Data.signExtend(imm, 21))
+    );
+  }
+
+}
