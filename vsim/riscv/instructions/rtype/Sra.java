0a8ac96942b90ed1c3c89b7b4c50d0bbaef3fc7b
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-08 13:25:55 -0600

    add rtype instructions

---
+++ vsim/riscv/instructions/rtype/Sra.java
@@ -0,0 +1,19 @@
+package vsim.riscv.instructions.rtype;
+
+
+public final class Sra extends RType {
+
+    public Sra() {
+        super(
+            "sra",
+            "sra rd, rs1, rs2",
+            "set rd = rs1 >> rs2[0:4], arithmetic shift right"
+        );
+    }
+
+    @Override
+    protected int compute(int rs1, int rs2) {
+        return rs1 >> (rs2 & 0x1f);
+    }
+
+}
