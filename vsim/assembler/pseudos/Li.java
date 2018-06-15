52315bdd3bf7a07586f13160c4906466269fdf13
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-14 19:43:14 -0600

    implement pseudos

--- vsim/assembler/pseudos/Li.java
+++ vsim/assembler/pseudos/Li.java
@@ -1,6 +1,8 @@
 package vsim.assembler.pseudos;
 
 import java.util.ArrayList;
+import vsim.assembler.statements.UType;
+import vsim.assembler.statements.IType;
 import vsim.assembler.statements.Statement;
 
 
@@ -15,8 +17,18 @@ public final class Li extends PSeudo {
     this.imm = imm;
   }
 
-  public ArrayList<Statement> build() {
-    return null;
+  public ArrayList<Statement> build(String filename) {
+    ArrayList<Statement> stmts = new ArrayList<Statement>();
+    if (imm > 2047 || imm < -2048) {
+      int imm_hi = this.imm >>> 12;
+      int imm_lo = this.imm & 0xfff;
+      stmts.add(new UType(filename, "lui", this.rd, imm_hi));
+      stmts.add(new IType(filename, "addi", this.rd, this.rd, imm_lo));
+    } else {
+      stmts.add(new IType(filename, "addi", this.rd, "x0", this.imm));
+    }
+    stmts.trimToSize();
+    return stmts;
   }
 
 }
--- vsim/assembler/pseudos/PSeudo.java
+++ vsim/assembler/pseudos/PSeudo.java
@@ -12,7 +12,7 @@ public abstract class PSeudo {
     this.name = name;
   }
 
-  public abstract ArrayList<Statement> build();
+  public abstract ArrayList<Statement> build(String filename);
 
   public String getName() {
     return this.name;
