bd8b06b3f548f4af903e818df202bc8b2ca809b8
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-16 10:00:36 -0600

    add debug setting
    Former-commit-id: 5a63a65cf96ae024651b4009adaf40a9aaf20fff

--- vsim/Settings.java
+++ vsim/Settings.java
@@ -3,7 +3,13 @@ package vsim;
 
 public final class Settings {
 
+  // bare machine no pseudos
   public static boolean BARE = false;
+
+  // dont show warnings
   public static boolean QUIET = false;
+
+  // debug
+  public static boolean DEBUG = false;
 
 }
