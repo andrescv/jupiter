b605933dda0f23421bad51222623c4013657a519
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-20 21:11:45 -0600

    add tests for add instruction

---
+++ test/add/test19.s
@@ -0,0 +1,25 @@
+.globl main
+
+.text
+
+main:
+  li x4, 0
+l1:
+  li x1, 0x0000000d
+  li x2, 0x0000000b
+  add x30, x1, x2
+  addi x6, x30, 0
+  addi x4, x4, 1
+  li x5, 2
+  bne x4, x5, l1
+  li x29, 0x00000018
+  bne x6, x29, fail
+
+sucess:
+  li a0, 10
+  ecall
+
+fail:
+  li a0, 17
+  li a1, 1
+  ecall
