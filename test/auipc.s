56dd4f740dd1f1bce6e63573949360ec9f77134a
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-21 20:03:35 -0600

    add auipc and lui tests

---
+++ test/auipc.s
@@ -0,0 +1,39 @@
+######################
+#    auipc TESTS     #
+######################
+
+.globl main
+
+.text
+
+main:
+
+test01:
+  li a1, 1
+  li x4, 0
+  li x5, 1
+label1_test01:
+  nop
+  nop
+  nop
+  beq x4, x5, label2_test01
+  li x4, 1
+  tail label1_test01
+label2_test01:
+  call label3_test01
+  j sucess
+  nop
+  nop
+  nop
+label3_test01:
+  nop
+  nop
+  ret
+
+sucess:
+  li a0, 10
+  ecall
+
+fail:
+  li a0, 17
+  ecall
