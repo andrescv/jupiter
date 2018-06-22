b8d1f342116d5f54069572d5818c6b654f5fc2c8
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-21 18:09:00 -0600

    update tests
    Former-commit-id: 9c46d6bf645c03db6ebfbb8f3be7fe1cffa6ffb7

--- test.py
+++ test.py
@@ -1,42 +1,45 @@
 import os
 import sys
+import subprocess
 from glob import glob
 
-
+newline = os.linesep
 CMD = 'java -jar VSim.jar -nocolor %s 1>/dev/null'
 
 
 def main():
-    error = False
-    print('')
-    print('Testing VSim....')
-    print('')
-    # just try to run every
+    print(newline + 'Testing VSim....' + newline)
+    # just try to run every program
     total = 0
     nerror = 0
     nsuccess = 0
     errors = []
-    for f in sorted(glob('test/*/*')):
-        total += 1
-        rcode = os.system(CMD % f)
-        if rcode != 0:
-            nerror += 1
-            error = True
-            print('    ' + f + ': ✘')
-            errors.append('    ' + f + ': ✘')
-        print('    ' + f + ': ✔')
-        nsuccess += 1
-    print('')
-    print('%d/%d tests passed' % (nsuccess, total))
-    if not error:
-        print('All done...')
-    else:
-        print('fail...')
-        print('')
-        print('Summary:')
-        print('')
-        for error in errors:
-            print(error)
+    try:
+        for f in sorted(glob('test/*')):
+            total += 1
+            command = CMD % f
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+            process.wait()
+            rcode = process.returncode
+            if rcode != 0:
+                nerror += 1
+                print('    ' + f + ':test%02d ✘' % rcode)
+                errors.append('    ' + f + ':test%02d ✘' % rcode)
+            else:
+                print('    ' + f + ': ✔')
+                nsuccess += 1
+        print(newline + '%d/%d tests passed' % (nsuccess, total))
+        if len(errors) == 0:
+            print(newline + 'All done...')
+        else:
+            print(newline + 'fail...' + newline)
+            print('Summary:' + newline)
+            for error in errors:
+                print(error)
+            print('')
+            sys.exit(1)
+    except KeyboardInterrupt:
+        print(newline + 'tests interrupted...' + newline)
         sys.exit(1)
 
 
