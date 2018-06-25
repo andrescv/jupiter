51b2b68f32e5fe562c92083b6c47d308b11d8311
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:53:09 -0600

    add r4type package documentation

--- vsim/riscv/instructions/r4type/Fnmsubs.java
+++ vsim/riscv/instructions/r4type/Fnmsubs.java
@@ -1,8 +1,33 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.r4type;
 
 
+/**
+ * The Fnmsubs class represents a fnmsub instruction.
+ */
 public final class Fnmsubs extends FR4Type {
 
+  /**
+   * Unique constructor that initializes a newly Fnmsubs object.
+   *
+   * @see vsim.riscv.instructions.r4type.FR4Type
+   */
   public Fnmsubs() {
     super(
       "fnmsub.s",
