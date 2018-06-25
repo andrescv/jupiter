3fb5a773eaea4faa5201aa0103e8696b389c4991
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 16:41:45 -0600

    add rtype package documentation

--- vsim/riscv/instructions/rtype/Fcvtsw.java
+++ vsim/riscv/instructions/rtype/Fcvtsw.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.rtype;
 
 import vsim.Globals;
@@ -7,8 +24,16 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The Fcvtsw class represents a fcvtsw instruction.
+ */
 public final class Fcvtsw extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly Fcvtsw object.
+   *
+   * @see vsim.riscv.instructions.Instruction
+   */
   public Fcvtsw() {
     super(
       Instruction.Format.R,
--- vsim/riscv/instructions/rtype/Fsubs.java
+++ vsim/riscv/instructions/rtype/Fsubs.java
@@ -1,8 +1,33 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.rtype;
 
 
+/**
+ * The Fsubs class represents a fsub instruction.
+ */
 public final class Fsubs extends FRType {
 
+  /**
+   * Unique constructor that initializes a newly Fsubs object.
+   *
+   * @see vsim.riscv.instructions.rtype.FRType
+   */
   public Fsubs() {
     super(
       "fsub.s",
