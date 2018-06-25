0944591c39d70271b75427533936a9d364a224a1
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:46:03 -0600

    add stype package documentation
    Former-commit-id: 636a5216aa1638a1bf345d678aa4f8d8dbbe9a6b

--- vsim/riscv/instructions/stype/SType.java
+++ vsim/riscv/instructions/stype/SType.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.stype;
 
 import vsim.Globals;
@@ -8,8 +25,18 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The class UType represents the general form of a s-type instruction.
+ */
 abstract class SType extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly SType object.
+   *
+   * @param mnemonic the instruction mnemonic
+   * @param usage the instruction usage
+   * @param description the instruction description
+   */
   protected SType(String mnemonic, String usage, String description) {
     super(Instruction.Format.S, mnemonic, usage, description);
   }
@@ -19,6 +46,13 @@ abstract class SType extends Instruction {
     return 0b0100011;
   }
 
+  /**
+   * This method sets the value of rs2 in memory[rs1 + imm]
+   *
+   * @param rs1 register source 1
+   * @param rs2 register source 2
+   * @param imm immediate offset
+   */
   protected abstract void setMemory(int rs1, int rs2, int imm);
 
   private int getImm(MachineCode code) {
