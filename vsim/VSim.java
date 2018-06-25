8fceb9e0f541389e817c5cc801520cf028ea0a84
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-24 19:46:39 -0600

    add Globals, Settings, VSim documentation

--- vsim/VSim.java
+++ vsim/VSim.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim;
 
 import vsim.utils.Cmd;
@@ -5,8 +22,19 @@ import vsim.utils.Message;
 import java.util.ArrayList;
 import vsim.simulator.Simulator;
 
+
+/**
+ * The VSim class contains the main method of VSim simulator.
+ */
 public final class VSim {
 
+  private VSim() { /* NOTHING */ }
+
+  /**
+   * This method is used to launch the VSim simulator.
+   *
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     // run this only if some argument(s) is/are passed
     if (args.length > 0) {
