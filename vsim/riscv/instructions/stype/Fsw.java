636a5216aa1638a1bf345d678aa4f8d8dbbe9a6b
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:46:03 -0600

    add stype package documentation

--- vsim/riscv/instructions/stype/Fsw.java
+++ vsim/riscv/instructions/stype/Fsw.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.stype;
 
 import vsim.Globals;
@@ -8,8 +25,16 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The Fsw class represents a fsw instruction.
+ */
 public final class Fsw extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly Fsw object.
+   *
+   * @see vsim.riscv.instructions.Instruction
+   */
   public Fsw() {
     super(
       Instruction.Format.S,
@@ -29,6 +54,11 @@ public final class Fsw extends Instruction {
     return 0b010;
   }
 
+  /**
+   * This method gets the immediate value of an fsw instruction.
+   *
+   * @return the immediate value
+   */
   private int getImm(MachineCode code) {
     int imm_11_5 = code.get(InstructionField.IMM_11_5);
     int imm_4_0 = code.get(InstructionField.IMM_4_0);
--- vsim/riscv/instructions/stype/Sw.java
+++ vsim/riscv/instructions/stype/Sw.java
@@ -1,10 +1,35 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.stype;
 
 import vsim.Globals;
 
 
+/**
+ * The Sw class represents a sw instruction.
+ */
 public final class Sw extends SType {
 
+  /**
+   * Unique constructor that initializes a newly Sw object.
+   *
+   * @see vsim.riscv.instructions.stype.SType
+   */
   public Sw() {
     super(
       "sw",
