d1f041f8800c45476a6c4d7d8d24219fc4a0105e
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 10:24:11 -0600

    add assembler documentation
    Former-commit-id: 163daef9b141e482b6a897f3539f41099cb3f5ea

--- vsim/assembler/Sym.java
+++ vsim/assembler/Sym.java
@@ -1,30 +1,80 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.assembler;
 
 import vsim.utils.Colorize;
 
 
-public final class Sym {
+/**
+ * The class Sym is used to represent a segment symbol.
+ */
+final class Sym {
 
+  /** the segment this symbol belongs */
   private Segment segment;
+  /** the address attached to this symbol */
   private int address;
 
-  public Sym(Segment segment, int address) {
+  /**
+   * Unique constructor that initializes a newly Sym object.
+   *
+   * @param segment the segment this symbol belongs
+   * @param address the address attached to this symbol
+   * @see vsim.assembler.Segment
+   */
+  protected Sym(Segment segment, int address) {
     this.segment = segment;
     this.address = address;
   }
 
-  public void setAddress(int address) {
+  /**
+   * This method sets the address of this symbol.
+   *
+   * @param address the address to set
+   */
+  protected void setAddress(int address) {
     this.address = address;
   }
 
-  public Segment getSegment() {
+  /**
+   * This method returns the segment this symbol belongs.
+   *
+   * @see vsim.assembler.Segment
+   * @return the segment this symbol belongs
+   */
+  protected Segment getSegment() {
     return this.segment;
   }
 
-  public int getAddress() {
+  /**
+   * This method returns the address of this symbol.
+   *
+   * @return the address of this symbol
+   */
+  protected int getAddress() {
     return this.address;
   }
 
+  /**
+   * This method returns a String representation of a Sym object.
+   *
+   * @return the String representation
+   */
   @Override
   public String toString() {
     return String.format(
