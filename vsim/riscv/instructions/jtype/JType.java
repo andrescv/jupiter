6273a76b9476a18dea2908da086247b4ad4479ac
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:56:59 -0600

    add jtype package documentation

--- vsim/riscv/instructions/jtype/JType.java
+++ vsim/riscv/instructions/jtype/JType.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.jtype;
 
 import vsim.Globals;
@@ -8,14 +25,35 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The class JType represents the general form of a j-type instruction.
+ */
 abstract class JType extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly UType object.
+   *
+   * @param mnemonic the instruction mnemonic
+   * @param usage the instruction usage
+   * @param description the instruction description
+   */
   protected JType(String mnemonic, String usage, String description) {
     super(Instruction.Format.J, mnemonic, usage, description);
   }
 
+  /**
+   * This method computes the result of the instruction.
+   *
+   * @param imm immediate value
+   * @return the computed result
+   */
   protected abstract int compute(int imm);
 
+  /**
+   * This method gets the immediate value of the instruction.
+   *
+   * @return the immediate value
+   */
   private int getImm(MachineCode code) {
     int imm_10_1 = code.get(InstructionField.IMM_10_1);
     int imm_11 = code.get(InstructionField.IMM_11J);
