1e7447771e8cc8fae0284d2ea9859f2d90531199
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:40:08 -0600

    add utype package documentation
    Former-commit-id: 7983e8e5b1e77f3beb102d4b5cd8ad5efc83c480

--- vsim/riscv/instructions/utype/UType.java
+++ vsim/riscv/instructions/utype/UType.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.utype;
 
 import vsim.Globals;
@@ -7,12 +24,28 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The class UType represents the general form of a u-type instruction.
+ */
 abstract class UType extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly UType object.
+   *
+   * @param mnemonic the instruction mnemonic
+   * @param usage the instruction usage
+   * @param description the instruction description
+   */
   protected UType(String mnemonic, String usage, String description) {
     super(Instruction.Format.U, mnemonic, usage, description);
   }
 
+  /**
+   * This method computes the result of the instruction.
+   *
+   * @param imm register source 1
+   * @return the computed result
+   */
   protected abstract int compute(int imm);
 
   @Override
