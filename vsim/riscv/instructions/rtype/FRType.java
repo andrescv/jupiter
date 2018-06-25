903af992bf18dff74769e2cb8f2c29c6d2739331
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 16:41:45 -0600

    add rtype package documentation
    Former-commit-id: 3fb5a773eaea4faa5201aa0103e8696b389c4991

--- vsim/riscv/instructions/rtype/FRType.java
+++ vsim/riscv/instructions/rtype/FRType.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.rtype;
 
 import vsim.Globals;
@@ -7,12 +24,30 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The class FRType represents the general form of a floating point
+ * r-type instruction.
+ */
 abstract class FRType extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly BType object.
+   *
+   * @param mnemonic the instruction mnemonic
+   * @param usage the instruction usage
+   * @param description the instruction description
+   */
   protected FRType(String mnemonic, String usage, String description) {
     super(Instruction.Format.R, mnemonic, usage, description);
   }
 
+  /**
+   * This method computes the result of the instruction.
+   *
+   * @param rs1 register source 1
+   * @param rs2 register source 2
+   * @return the computed result
+   */
   protected abstract float compute(float rs1, float rs2);
 
   @Override
--- vsim/riscv/instructions/rtype/Fmvxw.java
+++ vsim/riscv/instructions/rtype/Fmvxw.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.rtype;
 
 import vsim.Globals;
@@ -7,8 +24,16 @@ import vsim.riscv.instructions.Instruction;
 import vsim.riscv.instructions.InstructionField;
 
 
+/**
+ * The Fmvxw class represents a fmvxw/fmvxs instruction.
+ */
 public final class Fmvxw extends Instruction {
 
+  /**
+   * Unique constructor that initializes a newly Fmvxw object.
+   *
+   * @see vsim.riscv.instructions.Instruction
+   */
   public Fmvxw() {
     super(
       Instruction.Format.R,
