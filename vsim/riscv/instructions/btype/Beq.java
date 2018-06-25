f3f4ed391ad25c3d074f252f5dd20bef43320e9b
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-25 15:34:28 -0600

    add btype package documentation
    Former-commit-id: ccd52b113466dc3cbfa394789dbb2b758b581723

--- vsim/riscv/instructions/btype/Beq.java
+++ vsim/riscv/instructions/btype/Beq.java
@@ -1,8 +1,33 @@
+/*
+Copyright (C) 2018 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package vsim.riscv.instructions.btype;
 
 
+/**
+ * The Beq class represents a beq instruction.
+ */
 public final class Beq extends BType {
 
+  /**
+   * Unique constructor that initializes a newly Beq object.
+   *
+   * @see vsim.riscv.instructions.btype.BType
+   */
   public Beq() {
     super(
       "beq",
