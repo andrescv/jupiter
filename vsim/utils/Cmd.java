8a8996e1938d8d819c5e81abf0238401879b6fa3
Andrés Castellanos <andres.cv@galileo.edu>
2018-06-28 16:33:08 -0600

    add a more powerful argument parser
    Former-commit-id: 14895c38497c51707fddc2d6c64a85e048f6979f

--- vsim/utils/Cmd.java
+++ vsim/utils/Cmd.java
@@ -19,8 +19,6 @@ package vsim.utils;
 
 import vsim.Settings;
 import java.util.ArrayList;
-import java.io.StringReader;
-import java.io.BufferedReader;
 
 
 /**
@@ -28,119 +26,92 @@ import java.io.BufferedReader;
  */
 public final class Cmd {
 
-  private Cmd() { /* NOTHING */}
-
-  // newline
-  private static final String newline = System.getProperty("line.separator");
-
-  // VSim header
-  private static final String HEADER = "__   _____ _" + newline +
-                                       "\\ \\ / / __(_)_ ___" + newline +
-                                       " \\ V /\\__ \\ | '   \\" + newline +
-                                       "  \\_/ |___/_|_|_|_|";
-
-  // slogan and license
-  private static final String SUBHEADER = "RISC-V Assembler & Runtime Simulator" + newline +
-                                          newline + "GPL-3.0 License" + newline +
-                                          "Copyright (c) 2018 Andres Castellanos" + newline +
-                                          "All Rights Reserved." + newline +
-                                          "See the file LICENSE for a full copyright notice." + newline;
-
-  // usage [-h]
-  private static final String USAGE = "usage: vsim [flags] <files>" +
-                                      newline + newline + "optional flags:" + newline +
-                                      "  -h        show this help message and exit" + newline +
-                                      "  -bare     bare machine (no pseudo-ops)" + newline +
-                                      "  -quiet    do not print warnings" + newline +
-                                      "  -nocolor  do not colorize output (only on linux)" + newline +
-                                      "  -debug    start the debugger" + newline +
-                                      "  -version  display the current version of the simulator";
+  private Cmd() { /* NOTHING */ }
 
   /**
    * This method implements a simple argument parser.
    *
    * @param args arguments to parse
-   * @return an ArrayList of RISC-V assembler filenames to simulate (if any)
+   * @return an array of RISC-V assembler filenames to simulate (if any)
    */
   public static ArrayList<String> parse(String[] args) {
-    int lastArg = 0;
-    int firstFile = Math.max(0, args.length - 1);
-    for (int i = 0; i < args.length; i++) {
-      if (args[i].startsWith("-")) {
-        lastArg = i;
-        String option = args[i].substring(1);
-        if (option.equals("h")) {
-          Cmd.title();
-          Cmd.exit();
-        }
-        else if (option.equals("bare"))
-          Settings.BARE = true;
-        else if (option.equals("quiet"))
-          Settings.QUIET = true;
-        else if (option.equals("nocolor"))
-          Settings.COLORIZE = false;
-        else if (option.equals("debug"))
-          Settings.DEBUG = true;
-        else if (option.equals("version")) {
-          System.out.println(Settings.VERSION);
-          System.exit(0);
-        } else {
-          Cmd.title();
-          Message.warning("unknown argument: " + args[i] + newline);
-          Cmd.exit();
-        }
-      } else
-        firstFile = Math.min(firstFile, i);
+    ArgumentParser parser = new ArgumentParser();
+    // simulator available options
+    parser.add("-help",    "show this help message and exit");
+    parser.add("-bare",    "bare machine (no pseudo-ops)");
+    parser.add("-quiet",   "do not print warnings");
+    parser.add("-nocolor", "do not colorize output");
+    parser.add("-notitle", "do not print title and copyright notice");
+    parser.add("-dump",    "dump machine code to a file", true);
+    parser.add("-start",   "start program at global label (default: main)", true);
+    parser.add("-debug",   "start the debugger");
+    parser.add("-version", "show the simulator version and exit");
+    // parse args
+    parser.parse(args);
+    // override default Settings
+    if (parser.hasFlag("-bare"))
+      Settings.BARE = true;
+    if (parser.hasFlag("-quiet"))
+      Settings.QUIET = true;
+    if (parser.hasFlag("-nocolor"))
+      Settings.COLORIZE = false;
+    if (parser.hasFlag("-notitle"))
+      Settings.TITLE = false;
+    if (parser.hasFlag("-dump"))
+      Settings.DUMP = parser.value("-dump");
+    if (parser.hasFlag("-start"))
+      Settings.START = parser.value("-start");
+    if (parser.hasFlag("-debug"))
+      Settings.DEBUG = true;
+    // display usage if errors
+    if (parser.hasErrors()) {
+      Cmd.title();
+      for (String error: parser.getErrors())
+        Message.warning(error);
+      System.out.println();
+      parser.print();
+      System.exit(1);
     }
-    if (firstFile < lastArg) {
-      Message.warning("unexpected argument: " + args[firstFile] + newline);
-      Cmd.exit();
+    // first -help
+    if (parser.hasFlag("-help")) {
+      Cmd.title();
+      parser.print();
+      System.exit(0);
     }
-    ArrayList<String> files = new ArrayList<String>();
-    for (int i = firstFile; i < args.length; i++) {
-      if (!args[i].startsWith("-"))
-        files.add(args[i]);
+    // then -version
+    if (parser.hasFlag("-version")) {
+      System.out.println(Settings.VERSION);
+      System.exit(0);
     }
-    files.trimToSize();
-    return files;
+    return parser.targets();
   }
 
   /**
    * This method prints the title of the VSim simulator.
    */
   public static void title() {
-    if (Settings.COLORIZE) {
-      try {
-        String out = "";
-        BufferedReader br = new BufferedReader(new StringReader(Cmd.HEADER));
-        out += Colorize.red(br.readLine()) + newline;
-        out += Colorize.green(br.readLine()) + newline;
-        out += Colorize.blue(br.readLine()) + newline;
-        out += Colorize.yellow(br.readLine()) + newline + newline;
-        out += Colorize.cyan(Cmd.SUBHEADER.substring(0, 37));
-        out += Cmd.SUBHEADER.substring(37);
-        System.out.println(out);
-        return;
-      } catch (Exception e) {/* DO NOTHING */}
+    if (Settings.TITLE) {
+      String newline = System.getProperty("line.separator");
+      // cool title :]
+      System.out.println(Colorize.red(" _   __    _____"));
+      System.out.println(Colorize.green("| | / /___/ __(_)_ _"));
+      System.out.println(Colorize.blue("| |/ /___/\\ \\/ /  ' \\"));
+      System.out.println(Colorize.yellow("|___/   /___/_/_/_/_/" + newline));
+      System.out.println(Colorize.cyan("RISC-V Assembler & Runtime Simulator" + newline));
+      System.out.println("GPL-3.0 License");
+      System.out.println("Copyright (c) 2018 Andres Castellanos");
+      System.out.println("All Rights Reserved.");
+      System.out.println("See the file LICENSE for a full copyright notice." + newline);
     }
-    // if error/exception print title without color
-    System.out.println(Cmd.HEADER + newline);
-    System.out.println(Cmd.SUBHEADER);
   }
 
   /**
    * This method prints the prompt of the VSim simulator.
    */
   public static void prompt() {
-    System.out.print(Colorize.red(">") + Colorize.green(">") + Colorize.blue("> "));
-  }
-
-  /**
-   * This method prints the usage of the VSim simulator and then exits.
-   */
-  public static void exit() {
-    System.out.println(Cmd.USAGE);
-    System.exit(0);
+    System.out.print(Colorize.red(">"));
+    System.out.print(Colorize.green(">"));
+    System.out.print(Colorize.blue(">") + " ");
   }
 
 }
