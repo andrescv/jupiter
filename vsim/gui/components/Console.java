e73a42a138e150ad6f9cd5dd0ec8a77932caa9d0
Andrés Castellanos <andres.cv@galileo.edu>
2018-11-25 18:01:09 -0600

    update Console component

--- vsim/gui/components/Console.java
+++ vsim/gui/components/Console.java
@@ -1,9 +1,14 @@
 package vsim.gui.components;
 
 import java.io.PrintStream;
+import javafx.scene.image.Image;
+import javafx.scene.input.Clipboard;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import vsim.gui.utils.CustomInputStream;
+import javafx.scene.control.ContextMenu;
 import vsim.gui.utils.CustomOutputStream;
+import javafx.scene.input.ClipboardContent;
 
 
 /**
@@ -11,8 +16,6 @@ import vsim.gui.utils.CustomOutputStream;
  */
 public final class Console extends TextArea {
 
-  /** Standard input */
-  private CustomInputStream stdin;
   /** Standard output */
   private PrintStream stdout;
   /** Standard error */
@@ -23,19 +26,34 @@ public final class Console extends TextArea {
    */
   public Console() {
     super();
-    this.stdin = new CustomInputStream(this);
+    this.setEditable(false);
     this.stdout = new PrintStream(new CustomOutputStream(this));
     this.stderr = this.stdout;
-    this.getStyleClass().add("console");
-  }
-
-  /**
-   * Gets standard input of control.
-   *
-   * @return standard input
-   */
-  public CustomInputStream getStdin() {
-    return this.stdin;
+    // clear option
+    MenuItem clear = new MenuItem("clear");
+    clear.setOnAction(e -> this.setText(""));
+    ImageView clearImg = new ImageView();
+    clearImg.setFitWidth(20.0);
+    clearImg.setFitHeight(20.0);
+    clearImg.setImage(new Image(getClass().getResourceAsStream("/resources/img/icons/clear.png")));
+    clear.setGraphic(clearImg);
+    // copy option
+    MenuItem copy = new MenuItem("copy");
+    copy.setOnAction(e -> {
+      Clipboard clipboard = Clipboard.getSystemClipboard();
+      ClipboardContent content = new ClipboardContent();
+      content.putString(this.getText());
+      clipboard.setContent(content);
+    });
+    ImageView copyImg = new ImageView();
+    copyImg.setFitWidth(20.0);
+    copyImg.setFitHeight(20.0);
+    copyImg.setImage(new Image(getClass().getResourceAsStream("/resources/img/icons/copy.png")));
+    copy.setGraphic(copyImg);
+    // set context menu with this options
+    ContextMenu menu = new ContextMenu();
+    menu.getItems().addAll(clear, copy);
+    this.setContextMenu(menu);
   }
 
   /**
