c065b7ecc384be660b71a24b094f3db689953b03
Andrés Castellanos <andres.cv@galileo.edu>
2018-12-11 19:08:55 -0600

    update InfoStatement and LinkedProgram

--- vsim/linker/InfoStatement.java
+++ vsim/linker/InfoStatement.java
@@ -4,6 +4,7 @@ import vsim.Globals;
 import vsim.riscv.instructions.MachineCode;
 import vsim.assembler.statements.Statement;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
 
 /**
@@ -11,6 +12,10 @@ import javafx.beans.property.SimpleStringProperty;
  */
 public final class InfoStatement {
 
+  /** if the statement is a ebreak statement */
+  private final boolean ebreak;
+  /** if the statement has a breakpoint */
+  private final SimpleBooleanProperty breakpoint;
   /** address where the statement was placed in memory */
   private final SimpleStringProperty address;
   /** machine code of the statement */
@@ -28,10 +33,48 @@ public final class InfoStatement {
    */
   public InfoStatement(int address, Statement stmt) {
     MachineCode result = stmt.result();
+    this.ebreak = stmt.getMnemonic().equals("ebreak");
+    this.breakpoint = new SimpleBooleanProperty(this.ebreak);
     this.address = new SimpleStringProperty(String.format("0x%08x", address));
     this.machineCode = new SimpleStringProperty(result.toString());
     this.sourceCode = new SimpleStringProperty(stmt.getDebugInfo().getSource());
     this.basicCode = new SimpleStringProperty(Globals.iset.get(stmt.getMnemonic()).disassemble(result));
+  }
+
+  /**
+   * Gets the breakpoint property.
+   *
+   * @return breakpoint property
+   */
+  public SimpleBooleanProperty breakpointProperty() {
+    return this.breakpoint;
+  }
+
+  /**
+   * Gets the breakpoint property value.
+   *
+   * @return true if the statement has a breakpoint, false if not
+   */
+  public boolean getBreakpoint() {
+    return this.breakpoint.get();
+  }
+
+  /**
+   * Gets if the statement is a ebreak statement.
+   *
+   * @return true if the statement is a ebreak, false if not
+   */
+  public boolean isEbreak() {
+    return this.ebreak;
+  }
+
+  /**
+   * Sets breakpoint property boolean value.
+   *
+   * @param val boolean value
+   */
+  public void setBreakpoint(boolean val) {
+    this.breakpoint.set(val);
   }
 
   /**
