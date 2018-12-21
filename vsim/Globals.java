0f8d8366fd21525938d5e783615af98ef5a65757
Andrés Castellanos <andres.cv@galileo.edu>
2018-12-21 11:13:26 -0600

    use atomic boolean for Globals.exit
    Former-commit-id: 606a533f40489be305fd1b0dcd71f9b3244543e5

--- vsim/Globals.java
+++ vsim/Globals.java
@@ -24,6 +24,7 @@ import vsim.riscv.InstructionSet;
 import vsim.riscv.RVIRegisterFile;
 import vsim.riscv.RVFRegisterFile;
 import vsim.assembler.SymbolTable;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 /**
@@ -32,7 +33,7 @@ import vsim.assembler.SymbolTable;
 public final class Globals {
 
   /** If user use the exit/exit2 ecall */
-  public static boolean EXIT = false;
+  public static final AtomicBoolean exit = new AtomicBoolean(false);
 
   /** RISC-V (RV32) principal memory (RAM) */
   public static final Memory memory = Memory.ram;
@@ -76,7 +77,7 @@ public final class Globals {
    * This method reset the simulator state and global and local symbol tables
    */
   public static void reset() {
-    Globals.EXIT = false;
+    Globals.exit.set(false);
     Errors.clear();
     Globals.resetST();
     Globals.resetState();
