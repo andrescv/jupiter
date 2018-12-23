b80b9ae2714e245981f0607618f58864c6d7e3b6
Andrés Castellanos <andres.cv@galileo.edu>
2018-12-23 17:31:08 -0600

    improve debugger and add simulation status class

---
+++ vsim/simulator/Status.java
@@ -0,0 +1,25 @@
+package vsim.simulator;
+
+import javafx.beans.property.SimpleBooleanProperty;
+
+
+public final class Status {
+
+  /** if program is assembled and ready to simulate */
+  public static final SimpleBooleanProperty READY = new SimpleBooleanProperty(false);
+
+  /** if simulation is running */
+  public static final SimpleBooleanProperty RUNNING = new SimpleBooleanProperty(false);
+
+  /** if simulation executes an exit/exit2 ecall */
+  public static final SimpleBooleanProperty EXIT = new SimpleBooleanProperty(false);
+
+  /**
+   * Resets all status flags.
+   */
+  public static void reset() {
+    Status.RUNNING.set(false);
+    Status.EXIT.set(false);
+  }
+
+}
