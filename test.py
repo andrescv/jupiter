88536754dfdd59847a151f10f099960e6e93807f
Andrés Castellanos <andres.cv@galileo.edu>
2019-01-13 10:23:48 -0600

    clean project structure
    Former-commit-id: 3eda3d081858b50148977a70f177f34cfb4520b9

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ def main():
     nsuccess = 0
     errors = []
     try:
-        for f in sorted(glob('test/riscv-tests/*')):
+        for f in sorted(glob('riscv-tests/*')):
             # ignore readme and license files
             if not f.endswith('.s'):
                 continue
