4864d5b18f754f86d45ec820d4dd11472068f452
Andrés Castellanos <andres.cv@galileo.edu>
2019-02-03 13:15:14 -0600

    remove unused import
    Former-commit-id: 215dbf2c85f6f1082496c626d95f65eec7d335b4

--- V-Sim/src/vsim/gui/components/AboutDialog.java
+++ V-Sim/src/vsim/gui/components/AboutDialog.java
@@ -24,7 +24,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import com.jfoenix.controls.JFXDialog;
 import vsim.Globals;
 
