8a58b062fc1581ec8575edb46edfed08296ed796
Andrés Castellanos <andres.cv@galileo.edu>
2019-02-06 14:59:43 -0600

    update minor version
    Former-commit-id: ce0950fb6c58e3e5ce88679b3bbbfc44d7f0d369

--- V-Sim/src/vsim/Globals.java
+++ V-Sim/src/vsim/Globals.java
@@ -32,7 +32,7 @@ import vsim.riscv.hardware.RVIRegisterFile;
 public final class Globals {
 
   /** V-Sim current version */
-  public static final String VERSION = "v2.0.0";
+  public static final String VERSION = "v2.0.1";
 
   /** V-Sim Copyright */
   public static final String COPYRIGHT = "Copyright (c) 2018-2019 Andres Castellanos";
