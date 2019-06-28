4a22235464bad181656f1d6d4a90c39899cd3d73
Andres Castellanos <andres.cv@galileo.edu>
2019-06-27 18:00:54 -0600

    fix RISC-V tests path
    fix RISC-V tests path
    
    fix RISC-V tests path
    
    
    Former-commit-id: 4f3be8354d8aeaddbc81416170bbca14f8e52649

--- src/scripts/rvtests.py
+++ src/scripts/rvtests.py
@@ -32,7 +32,7 @@ def main():
     nsuccess = 0
     errors = []
     try:
-        for f in sorted(glob('./src/test/riscv-tests/*')):
+        for f in sorted(glob(os.path.join('src', 'test', 'riscv-tests', '*'))):
             # ignore readme and license files
             if not f.endswith('.s'):
                 continue
