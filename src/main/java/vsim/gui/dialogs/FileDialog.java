1e4ddcd0e00f975c0edaef7ebf0f03ad4327f9f3
Andres Castellanos <andres.cv@galileo.edu>
2019-06-29 20:21:01 -0600

    remove DeleteDialog and add FileDialog
    Former-commit-id: 236ca8c70129bc34ac7cd8b62ab19c2c57a0a010

---
+++ src/main/java/vsim/gui/dialogs/FileDialog.java
@@ -0,0 +1,76 @@
+/*
+Copyright (C) 2018-2019 Andres Castellanos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+package vsim.gui.dialogs;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+
+/** V-Sim file dialog. */
+public final class FileDialog {
+
+  private final Stage stage;
+  /** file chooser */
+  private final FileChooser chooser;
+
+  /**
+   * Creates a new file dialog.
+   *
+   * @param stage dialog stage
+   */
+  public FileDialog(Stage stage) {
+    this.stage = stage;
+    chooser = new FileChooser();
+    chooser.getExtensionFilters().add(new ExtensionFilter("RISC-V Files", "*.s", "*.asm"));
+  }
+
+  /**
+   * Shows open file dialog.
+   *
+   * @param title dialog title
+   */
+  public Path open(String title) {
+    chooser.setTitle(title);
+    File f = chooser.showOpenDialog(stage);
+    if (f != null) {
+      return f.toPath();
+    }
+    return null;
+  }
+
+  /**
+   * Shows save file dialog.
+   *
+   * @param title dialog title
+   * @param filename initial file name
+   */
+  public Path save(String title, String filename) {
+    chooser.setTitle(title);
+    chooser.setInitialFileName(filename);
+    File f = chooser.showSaveDialog(stage);
+    if (f != null) {
+      return f.toPath();
+    }
+    return null;
+  }
+
+}
