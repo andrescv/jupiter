32c2e8bc291fea146bad0072776686e5e0bfa500
Andres Castellanos <andres.cv@galileo.edu>
2019-07-04 12:28:01 -0600

    remove unnecessary status flags

--- src/main/java/vsim/gui/Status.java
+++ src/main/java/vsim/gui/Status.java
@@ -29,12 +29,6 @@ public final class Status {
   /** if simulation is running */
   public static final SimpleBooleanProperty RUNNING = new SimpleBooleanProperty(false);
 
-  /** if simulation is stopped by the user */
-  public static final SimpleBooleanProperty STOPPED = new SimpleBooleanProperty(false);
-
-  /** if simulation is handling an I/O request */
-  public static final SimpleBooleanProperty IO = new SimpleBooleanProperty(false);
-
   /** if simulation executes an exit/exit2 ecall */
   public static final SimpleBooleanProperty EXIT = new SimpleBooleanProperty(false);
 
@@ -45,11 +39,10 @@ public final class Status {
    * Resets all status flags.
    */
   public static void reset() {
+    Status.READY.set(false);
     Status.RUNNING.set(false);
     Status.EXIT.set(false);
     Status.EMPTY.set(true);
-    Status.STOPPED.set(false);
-    Status.IO.set(false);
   }
 
 }
