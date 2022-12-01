1dee328b268d8de05536fd15fd7bfa9e81bb29a0
Andrés Castellanos <me@andrescv.dev>
2022-12-01 17:35:39 -0600

    chore: add jupiter eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['jupiter'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/'],
+    },
+  },
+};
---
+++ packages/eslint-config-jupiter/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  extends: [
+    'next',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@next/next/no-html-link-for-pages': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
